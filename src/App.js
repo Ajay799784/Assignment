c065ddf82e25ef4d991daf134ecfe68ef431e9c4
yaram Ajay <102249358+Ajay799784@users.noreply.github.com>
2024-12-15 19:53:49 +0530

    Update App.js
    updating

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import Login from "./Components/LoginPage/Login";
-// import ForgotPassord from "./Components/ForgotPasswordPage/ForgotPassord";
-import MultiFormPage from "./Components/MultiForm/MultiFormPage";
+
 
 function App() {
   return (
     <div>
-      {/* <Login /> */}
-      {/* <ForgotPassord /> */}
-      <MultiFormPage />
+      <Login /> 
+      
+      
     </div>
   );
 }
